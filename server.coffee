c811dc02ffd2ee51bc8c8f3aa27ea998f7c88d01
Nino Paolo <o70vd@yahoo.com>
2013-05-12 11:17:10 +0800

    Added /js/getJson.js route

--- server.coffee
+++ server.coffee
@@ -1,5 +1,7 @@
 connect = require 'connect'
 http = require 'http'
+https = require 'https'
+url = require 'url'
 
 app = connect()
   .use(connect.favicon())
@@ -11,7 +13,32 @@ app = connect()
     secret: 'nodejspaolo'
   ))
   .use (req, res) ->
-    res.end "<h1>404 Not Found</h1>\n"
+    console.log req
+
+    if req._parsedUrl.pathname is "/js/getJson.js"
+      _url = url.parse req._parsedUrl.query
+      console.log _url
+      options =
+        host: _url.host
+        port: 443
+        path: _url.path
+        method: 'GET'
+
+      options.agent = new https.Agent options
+
+      req = https.request options, (response) ->
+        #console.log 'STATUS: ' + response.statusCode
+        #console.log 'HEADERS: ' + JSON.stringify response.headers
+        response.setEncoding 'utf8'
+        response.on 'data', (data) ->
+          #console.log 'BODY: ' + chunk
+          res.end data
+
+      req.on 'error', (e) ->
+        console.log 'problem with request: ' + e.message
+
+      req.end()
+    else
+      return res.end "<h1>404 Not Found</h1>\n"
 
 http.createServer(app).listen(3000)
-
--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 (function() {
-  var app, connect, http;
+  var app, connect, http, https, url;
 
   connect = require('connect');
 
   http = require('http');
 
+  https = require('https');
+
+  url = require('url');
+
   app = connect().use(connect.favicon()).use(connect.logger()).use(connect["static"](__dirname + '/public')).use(connect.directory(__dirname + '/public')).use(connect.cookieParser()).use(connect.session({
-    secret: 'playerfm@20130510@'
+    secret: 'nodejspaolo'
   })).use(function(req, res) {
-    return res.end("<h1>404 Not Found</h1>\n");
+    var options, _url;
+
+    console.log(req);
+    if (req._parsedUrl.pathname === "/js/getJson.js") {
+      _url = url.parse(req._parsedUrl.query);
+      console.log(_url);
+      options = {
+        host: _url.host,
+        port: 443,
+        path: _url.path,
+        method: 'GET'
+      };
+      options.agent = new https.Agent(options);
+      req = https.request(options, function(response) {
+        response.setEncoding('utf8');
+        return response.on('data', function(data) {
+          return res.end(data);
+        });
+      });
+      req.on('error', function(e) {
+        return console.log('problem with request: ' + e.message);
+      });
+      return req.end();
+    } else {
+      return res.end("<h1>404 Not Found</h1>\n");
+    }
   });
 
   http.createServer(app).listen(3000);
